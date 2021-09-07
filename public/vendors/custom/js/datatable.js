fc7495495cc31edb6c4d42f3646573ac6832ea12
KOMLANI Tibang Tibang\'be <tibang.kolani@gmail.com>
2021-09-07 14:38:08 +0000

    add datatable

---
+++ public/vendors/custom/js/datatable.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.datatable').DataTable({
+        "language":{
+         "url": "public/vendors/custom/js/fr_fr.json"
+        },
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+    });
+});
