e4e3968043e530224debf8058e235f1cc360b926
Richejoty <richejoy314@gmail.com>
2021-11-30 18:24:17 +0000

    Dev en cours

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -1,4 +1,4 @@
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('favicon.png') }}"
-        alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('favicon.jpg') }}"
+        alt="{{ config('app.name') }}" height="60" width="60">
 </div>
